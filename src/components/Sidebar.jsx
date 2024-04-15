85df05400930e7351832579c3b2bf7ffab5503cf
erik.minasian <erik.minasian@coliquio.de>
2024-04-15 12:05:52 +0400

    feat: styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
+import { mockProjects } from "../static/mockProjects";
+import { buttonStyle } from "../styles/buildBlocks";
+
 export default function Sidebar() {
   return (
-    <aside className="p-4 bg-slate-950 text-white">
-      <h2>Your Projects</h2>
-      <button>Add project</button>
+    <aside className="p-4 bg-slate-950 text-white mt-4 rounded-tr-lg min-w-80">
+      <h2 className="text-2xl font-bold">Your Projects</h2>
+      <button className={`${buttonStyle} bg-white text-slate-950`}>Add project</button>
     </aside>
   );
 }
---
+++ src/styles/buildBlocks.js
@@ -0,0 +1,1 @@
+export const buttonStyle = "text-sm font-semibold p-2 m-2 min-w-36 rounded-lg"
