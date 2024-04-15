ca11baf51de90a554a06827ffdfe76bdde72c144
erik.minasian <erik.minasian@coliquio.de>
2024-04-15 13:26:28 +0400

    feat: no project in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,19 @@ import { buttonStyle } from "../styles/buildBlocks";
 export default function Sidebar() {
   return (
     <aside className="p-4 bg-slate-950 text-white mt-4 rounded-tr-lg min-w-80 flex flex-col">
-      <h2 className="text-2xl font-bold">Your Projects</h2>
-      <button className={`${buttonStyle} bg-white text-slate-950 self-center`}>Add project</button>
+      <h2 className="text-2xl font-bold my-4">Your Projects</h2>
+      {mockProjects.length > 0 ? (
+        <ul>
+          {mockProjects.map((project) => (
+            <li></li>
+          ))}
+        </ul>
+      ) : (
+        <span className="self-center">No projects to display.</span>
+      )}
+      <button className={`${buttonStyle} bg-white text-slate-950 self-center`}>
+        Add project
+      </button>
     </aside>
   );
 }
