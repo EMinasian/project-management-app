5b26dbf54950fc6eff7ef365ab626117de5da6ab
erik.minasian <erik.minasian@coliquio.de>
2024-04-19 21:08:47 +0400

    feat: project overview styled

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,3 +1,10 @@
-export default function ProjectItem() {
-  return <li></li>;
+export default function ProjectItem({ project }) {
+  const { id, title, description, date } = project;
+  return (
+    <li key={id} className="p-2 m-6 bg-slate-200 max-w-[640px] rounded-xl">
+      <h3 className="text-xl font-semibold mb-3">{title}</h3>
+      <span className="text-xs italic">{date}</span>
+      <p>{description}</p>
+    </li>
+  );
 }
