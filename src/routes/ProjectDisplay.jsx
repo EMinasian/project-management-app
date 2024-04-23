fa651d1f4a25cc234ea612ce717f1cb56466a6a9
erik.minasian <erik.minasian@coliquio.de>
2024-04-23 13:03:53 +0400

    feat: solve params values problem

--- src/routes/ProjectDisplay.jsx
+++ src/routes/ProjectDisplay.jsx
@@ -1,17 +1,23 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
 import TasksSection from "../components/TasksSection";
+import { projectContext } from "../contexts/ProjectsProvider";
 
 export default function ProjectDisplay({ id }) {
-  // const { title, description, date } = project;
+  const { projects } = useContext(projectContext);
+  const { projectId } = useParams();
+  const { title, description, date } = projects.find(({ id }) => id === projectId);
+
   return (
     <>
-    <div>{id}</div>
-      {/* <section>
+      <div>{id}</div>
+      <section>
         <h2>{title}</h2>
         <p>{description}</p>
         <span>{date}</span>
         <button>Delete</button>
       </section>
-      <TasksSection /> */}
+      <TasksSection />
     </>
   );
 }
